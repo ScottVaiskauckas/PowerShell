2a139e750a9034c998bf51b730d1f652fc2ca287
Scott Vaiskauckas <39973823+ScottVaiskauckas@users.noreply.github.com>
2020-10-29 15:34:36 -0500

    Made some changes to mattprintexample.ps1

--- Examples/MattPrintExample.ps1
+++ Examples/MattPrintExample.ps1
@@ -1,10 +1,19 @@
-$ParentDirectory = "C:\Users\Scott\Desktop\print_pdf"
-$PathToNetworkPrinter = "\\servername\printername"
-$Files = Get-ChildItem $ParentDirectory
+# This is the folder you want to get the files from. You'll need to change this.
+# This is pointing to a folder named PDF in the signed in user's documents.
+$ParentDirectory = "$env:userprofile\Documents\PDF"
 
+# This gets the file names from the parent directory
+# Pipes that into Where-Object which only selects files with the pdf extension
+# Then pipes that into Sort-Object which sorts the names in ascending order
+$Files = Get-ChildItem $ParentDirectory | 
+Where-Object -Property Name -Like *.pdf | 
+Sort-Object
+
+# Iterates over the items in the $Files array
 Foreach($File in $Files)
 {
     $FileName = $File.name
-
-    Start-Process "$ParentDirectory\$FileName" -verb printto $PathToNetworkPrinter
+    # Should print the file using your default printer, but you might need to pipe this into kill.
+    # I can help with that. See here: 
+    Start-Process "$ParentDirectory\$FileName" -verb print
 }
