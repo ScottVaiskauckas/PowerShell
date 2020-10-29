1b49ee82a49602520aad1583c750264a67d93150
Scott Vaiskauckas <39973823+ScottVaiskauckas@users.noreply.github.com>
2020-10-29 15:36:44 -0500

    more changes to the printing script

--- Examples/MattPrintExample.ps1
+++ Examples/MattPrintExample.ps1
@@ -14,6 +14,6 @@ Foreach($File in $Files)
 {
     $FileName = $File.name
     # Should print the file using your default printer, but you might need to pipe this into kill.
-    # I can help with that. See here: 
+    # I can help with that. See here: https://gregcaporale.wordpress.com/2012/01/18/powershell-to-print-files-automatically/
     Start-Process "$ParentDirectory\$FileName" -verb print
 }
