52d13ebe50947ec0922e2047e6541088b915779a
Scott Vaiskauckas <39973823+ScottVaiskauckas@users.noreply.github.com>
2020-10-29 13:50:29 -0500

    updated mattprintexample.ps1

--- Examples/MattPrintExample.ps1
+++ Examples/MattPrintExample.ps1
@@ -1,9 +1,10 @@
 $ParentDirectory = "C:\Users\Scott\Desktop\print_pdf"
+$PathToNetworkPrinter = "\\servername\printername"
 $Files = Get-ChildItem $ParentDirectory
 
 Foreach($File in $Files)
 {
     $FileName = $File.name
 
-    Start-Process "$ParentDirectory\$FileName" -verb print
+    Start-Process "$ParentDirectory\$FileName" -verb printto $PathToNetworkPrinter
 }
